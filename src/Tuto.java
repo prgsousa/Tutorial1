f0903ed9321586e5dfc5acbccb8e113d2294c727
Rafael <Rafael@rafael-pc>
2013-08-29 09:06:32 -0300

    Definição inicial do projeto.

---
+++ src/Tuto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael
+ */
+public class Tuto {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+    
+}
